8da4eba50a218f9f5192e8cdf39a42e0b199bf57
tangculy <admin@vlanl.com>
2025-12-29 20:23:07 +0800

    settings_page.dart

--- flutter/lib/mobile/pages/settings_page.dart
+++ flutter/lib/mobile/pages/settings_page.dart
@@ -837,7 +837,7 @@ class _SettingsState extends State<SettingsPage> with WidgetsBindingObserver {
                 title: Text(translate("Version: ") + version),
                 value: Padding(
                   padding: EdgeInsets.symmetric(vertical: 8),
-                  child: Text('rustdesk.com',
+                  child: Text('vlanl.com',
                       style: TextStyle(
                         decoration: TextDecoration.underline,
                       )),
